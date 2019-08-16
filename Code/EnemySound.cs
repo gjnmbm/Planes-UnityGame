c72a589a1b0ab5dbaaec0ba5936e1a341e49b9d8
gjnmbm <53543842+gjnmbm@users.noreply.github.com>
2019-08-16 08:56:16 -0700

    Add files via upload

---
+++ Code/EnemySound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour
+{
+    public static AudioClip explosion;
+    static AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosion = Resources.Load<AudioClip>("Explosion Noise");
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip) {
+        if (clip.Equals("explosion")) {
+            audio.PlayOneShot(explosion); 
+        }
+    }
+}
